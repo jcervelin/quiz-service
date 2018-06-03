ceed2ca617ee89cc8abd3aa21380d488d3ee4415
Juliano de Sousa Cervelin <juliano.cervelin@gmail.com>
2018-06-03 14:06:59 -0300

    Adjustments in heroku configs

--- src/main/java/io/jcervelin/ideas/QuizServiceApplication.java
+++ src/main/java/io/jcervelin/ideas/QuizServiceApplication.java
@@ -3,11 +3,9 @@ package io.jcervelin.ideas;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class QuizServiceApplication implements CommandLineRunner{
 	/*@Autowired
 	private QuizServiceRepository repo;*/
