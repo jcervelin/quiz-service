276b3bdad5143214ede1ba530679e3c69fde0fd3
Juliano de Sousa Cervelin <juliano.cervelin@gmail.com>
2018-06-03 14:39:20 -0300

    Adjustments in heroku configs

--- src/main/java/io/jcervelin/ideas/entities/StateEnum.java
+++ src/main/java/io/jcervelin/ideas/entities/StateEnum.java
@@ -1,38 +1,33 @@
 package io.jcervelin.ideas.entities;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public enum StateEnum {
 	
-	RO(11,"RondÙnia"),
+	RO(11,"Rond√¥nia"),
 	AC(12,"Acre"),
 	AM(13,"Amazonas"),
 	RR(14,"Roraima"),
-	PA(15,"Par·"),
-	AP(16,"Amap·"),
+	PA(15,"Par√°"),
+	AP(16,"Amap√°"),
 	TO(17,"Tocantins"),
-	MA(21,"Maranh„o"),
-	PI(22,"PiauÌ"),
-	CE(23,"Cear·"),
+	MA(21,"Maranh√£o"),
+	PI(22,"Piau√≠"),
+	CE(23,"Cear√°"),
 	RN(24,"Rio Grande do Norte"),
-	PB(25,"ParaÌba"),
+	PB(25,"Para√≠ba"),
 	PE(26,"Pernambuco"),
 	AL(27,"Alagoas"),
 	SE(28,"Sergipe"),
 	BA(29,"Bahia"),
 	MG(31,"Minas Gerais"),
-	ES(32,"EspÌrito Santo"),
+	ES(32,"Esp√≠rito Santo"),
 	RJ(33,"Rio de Janeiro"),
-	SP(35,"S„o Paulo"),
-	PR(41,"Paran·"),
+	SP(35,"S√£o Paulo"),
+	PR(41,"Paran√°"),
 	SC(42,"Santa Catarina"),
 	RS(43,"Rio Grande do Sul"),
 	MS(50,"Mato Grosso do Sul"),
 	MT(51,"Mato Grosso"),
-	GO(52,"Goi·s"),
+	GO(52,"Goi√°s"),
 	DF(53,"Distrito Federal");
 
 	private final int ibgeCod;
